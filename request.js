68f9e439c1ea32af78c1f63a1e3c55b736120ce3
Emran Khandaker Evan <evanemran007@gmail.com>
2022-06-09 17:04:35 +0600

    ui updated

--- request.js
+++ request.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { ImageBackground, StyleSheet, Text, View, Image } from "react-native";
 
-const image = { uri: "https://lh3.googleusercontent.com/37jKSwObSfyxrdC6jy69aEvqND1dsyh_z7-AKX43OgoF9rxKhlUyz_Jm4tYXSYXUa7o" };
+const image = { uri: "https://w0.peakpx.com/wallpaper/339/669/HD-wallpaper-mcu-avengers-birds-black-panther-heroes-marvel-samurai-spider-man-star-universe-villians.jpg" };
 
 class RequestManager extends Component {
    state = {
@@ -27,19 +27,33 @@ class RequestManager extends Component {
 
         <View style={styles.container}>
             <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+            <Text style={styles.mainTitle}>
+                The Next MCU Film
+                </Text>
                 <Text style={styles.mainTitle}>
                 {this.state.data.title}
                 </Text>
+
+                <Text style={styles.mainSubTitle}>
+                {"Releasing In " + this.state.data.days_until + " Days"}
+                </Text>
+
                 <Text style={styles.mainSubTitle}>
                 {"Release Date: "+this.state.data.release_date}
                 </Text>
 
-                <Text style={styles.mainSubTitle}>
-                {this.state.data.days_until + " Days Left"}
-                </Text>
+                <View style={styles.row_container}>
+                <Image style={{width: '50%', height: '100%'}}
+                    source={{uri: "https://upload.wikimedia.org/wikipedia/en/8/88/Thor_Love_and_Thunder_poster.jpeg"}}/>
+                </View>
+            
 
-                <Image style={{width: '50%', height: '50%'}}
-                    source={{uri:this.state.data.poster_url}}/>
+                <Text style={styles.mainSubTitle}>
+                   Overview
+                </Text>
+                <Text style={styles.overviewStyle}>
+                    {this.state.data.overview}
+                </Text>
             </ImageBackground>
         </View>
 
@@ -62,6 +76,12 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
     },
+    row_container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: "center",
+        backgroundColor: "#000000c0"
+      },
     image: {
       flex: 1,
       justifyContent: "flex-start"
@@ -69,15 +89,16 @@ const styles = StyleSheet.create({
     text: {
       color: "white",
       fontSize: 42,
-      lineHeight: 84,
+      lineHeight: 50,
       fontWeight: "bold",
       textAlign: "center",
       backgroundColor: "#000000c0"
     },
     mainTitle: {
-        color: "white",
+        color: "yellow",
         fontSize: 30,
-        lineHeight: 84,
+        lineHeight: 60,
+        fontFamily: 'acme.ttf',
         fontWeight: "bold",
         textAlign: "center",
         backgroundColor: "#000000c0"
@@ -85,8 +106,16 @@ const styles = StyleSheet.create({
     mainSubTitle: {
         color: "white",
         fontSize: 20,
-        lineHeight: 84,
+        lineHeight: 50,
         fontWeight: "bold",
+        textAlign: "center",
+        backgroundColor: "#000000c0"
+    },
+    overviewStyle: {
+        color: "grey",
+        fontSize: 18,
+        lineHeight: 25,
+        fontWeight: "normal",
         textAlign: "center",
         backgroundColor: "#000000c0"
     }
